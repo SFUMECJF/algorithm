15738003d5b646963ac29acd762224abafe8fdf3
SanFeng <zefenglie@163.com>
2021-03-26 21:44:21 +0800

    best binarysearch algorith for 2582.cpp

--- binarySearch/2582.cpp
+++ binarySearch/2582.cpp
@@ -1,92 +1,46 @@
-////
-//// Created by sf on 2021/3/25.
-////
-////#include <bits/stdc++.h>
-//#include <iostream>
-// using namespace std;
-//这里的long long int *elements需要和调用时候的数组类型相同。
- int binary_search(long long int *elements, int key, int low = 0, int high = 0)
- //需要排序好的数组
-{
-  int mid; //数据储存不一定是从0开始的。很大的数组，所以要传递数量low,high
-
-  while (low <= high) //注意要有等于，否则会漏掉正好在中间的值
-  {
-    mid = (low + high) / 2;
-    if (elements[mid] < key)
-      low = mid + 1;
-    else if (elements[mid] > key)
+#include<iostream>
+using namespace std;
+//数据储存不一定是从0开始的。很大的数组，所以要传递数量low,high
+int binary_search (long long *prefix_sum, int low, int high, long long value){
+  while (low <= high){//注意要有等于，否则会漏掉正好在中间的值
+    int mid = (low + high) / 2;
+    if (prefix_sum[mid] > value)
       high = mid - 1;
+    else if (prefix_sum[mid] < value)
+      low = mid + 1;
     else
       return mid;
+
   }
-  //从小到大递增的数组，找一个小于等于的前缀和，不能大于
-  //此时low > high
-  return high; //记住没找到返回什么
+  return high;//如果没找到，返回小的那个。不用判断为负的情况？最多为0，是不是？然后为0，相减说明i-j也是一个可行的区间。
 }
-//
-// int main() {
-//  long long s;
-//  int n;
-//  cin >> s >> n;
-//  int item[n], min_length = n + 1;
-//  long long prefix_sum[n + 1];
-//  prefix_sum[0] = 0;
-//
-//  for (int i = 0; i < n; i++) {
-//    cin >> item[i];
-//    prefix_sum[i + 1] = prefix_sum[i] + item[i];
-//  }
-//
-//  for (int i = 1; i <= n; i++) {
-//    if (prefix_sum[i] < s)
-//      continue;
-//    else {
-//      // int j = upper_bound(prefix_sum, prefix_sum + i, prefix_sum[i] - s) -
-//      prefix_sum;
-//      int j = binarySearch(prefix_sum, prefix_sum[i] - s, 1, i);
-//      min_length = min(min_length, i - j - 1);
-//    }
-//  }
-//  if (min_length == n + 1)
-//    cout << 0 << endl;
-//  else
-//    cout << min_length;
-//
-//  return 0;
-//}
-/*
- *
-50 20
-10 8 9 3 11 8 5 1 1 1 1 20 8 9 11 4 13 22 9 6
 
- output 4
+//long long prefix_sum[500005];//从1开始，到n储存a[0]到a[n - 1]
 
- */
-#include <bits/stdc++.h>
-using namespace std;
-#define MIN(a, b) a < b ? a : b
-int main() {
-  long long s;
-  int n;
+int main(){
+
+  int  s, n, a[500005];
   cin >> s >> n;
-  int a[n], res = n + 1;
-  long long sum[n + 1];
-  sum[0] = 0;
-  for (int i = 0; i < n; i++) {
+  int result = n + 1;//要最小结果，所以给大值
+  long long prefix_sum[n + 1];//从1开始，到n储存a[0]到a[n - 1]
+  //这里必须采用利用输入的n+1来定义long long的大小，一开始直接定义long long prefix_sum[500005]
+  //或者采用将long long prefix_sum[500005]的定义 放到主函数外围，则不会炸内存
+  prefix_sum[0] = 0;
+  for(int i = 0; i < n; i++){
     cin >> a[i];
-    sum[i + 1] = sum[i] + a[i];
+    prefix_sum[i + 1] = prefix_sum[i] + a[i];//
   }
-  for (int i = 1; i <= n; i++) {
-    if (sum[i] < s)
-      continue;
-    else {
-      //int j = upper_bound(sum, sum + i, sum[i] - s) - sum;
-      int j = binary_search(sum, sum[i] - s, 1, i);
-      res = MIN(res, i - j);
+
+  for(int i = 1; i <= n; i++){//i <= n 不是i < n
+    if(prefix_sum[i] >= s){
+      int j = binary_search(prefix_sum, 1, i, prefix_sum[i] - s);//返回坐标，坐标值应该是 <= prefix_sum[i] - s的，
+                                               // 如此才满足prefix_sum[i] -prefix_sum[j] >= s
+      result = min(result, i - j);
     }
   }
-  res = res == n + 1 ? 0 : res;
-  cout << res << endl;
+  if(result == n + 1)
+    cout << 0;
+  else
+    cout << result;
   return 0;
 }
